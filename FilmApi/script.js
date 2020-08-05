37b71c7a78c2af09d826e568d26dc54e4eaf8c18
Gor <Gor633369@mail.ru>
2020-08-05 16:15:26 +0400

    finished task 1&4

---
+++ FilmApi/script.js
@@ -0,0 +1,48 @@
+const filmContainer = document.querySelector(".film-container");
+
+fetch("https://ghibliapi.herokuapp.com/films")
+  .then((data) => data.json())
+  .then((data) => {
+    console.log(data);
+    data.forEach(({ title, description, director, producer, release_date }) => {
+      const newUl = createNewFilm([
+        title,
+        description,
+        director,
+        producer,
+        release_date,
+      ]);
+      filmContainer.append(newUl);
+    });
+  });
+
+/**
+ *
+ * @param {array} values - array of film descriptors
+ */
+function createNewFilm(values) {
+  const div = document.createElement("div");
+  const ul = document.createElement("ul");
+  ul.className = "film-info";
+  div.className = "film-list";
+
+  values.forEach((value) => {
+    const headings = [
+      "Title :",
+      "Descriptor :",
+      "Director :",
+      "Producer :",
+      "Release Date :",
+    ];
+    const li = document.createElement("li");
+    const h4 = document.createElement("h4");
+    const p = document.createElement("p");
+    p.innerText = value;
+    h4.innerText = headings[values.indexOf(value)];
+    li.append(h4);
+    li.append(p);
+    ul.append(li);
+  });
+  div.append(ul);
+  return div;
+}
